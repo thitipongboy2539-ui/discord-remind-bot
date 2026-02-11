013599d8f54b718ac1b43a95fd8dc16a0f64884a
thitipongboy2539-ui <thitipongboy2539@gmail.com>
2026-02-11 22:06:33 +0700

    Update web.py

--- web.py
+++ web.py
@@ -1,3 +1,10 @@
-git add .
-git commit -m "add web dashboard"
-git push
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "🚀 VIP BOT DASHBOARD RUNNING"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
