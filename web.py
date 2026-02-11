90f92c8bde2fba6170e0e9d4ee134cc4aed46e5b
thitipongboy2539-ui <thitipongboy2539@gmail.com>
2026-02-11 22:19:32 +0700

    Add files via upload

--- web.py
+++ web.py
@@ -7,4 +7,5 @@ def home():
     return "Bot Dashboard Running 👑"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
