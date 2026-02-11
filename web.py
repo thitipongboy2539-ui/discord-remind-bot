a9d5658740cad9d037130e54007a8ab1bd4287d6
thitipongboy2539-ui <thitipongboy2539@gmail.com>
2026-02-11 22:09:54 +0700

    Update web.py

--- web.py
+++ web.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 VIP BOT DASHBOARD RUNNING"
+    return "Bot Dashboard Running 👑"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
